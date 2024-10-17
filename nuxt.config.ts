568afd2e0d2dca44b95373ed2c22c4c231b8246d
Seth Brasile <seth.brasile@gmail.com>
2024-10-17 10:56:00 -0500

    upgrade nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
   modules: ["nuxt-icon"],
   compatibilityDate: "2024-10-17",
 });
