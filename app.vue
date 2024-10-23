cb5e95f0e367f4b7197c77af5737fa303cae40be
Seth Brasile <seth.brasile@gmail.com>
2024-10-23 15:20:31 -0500

    add HS and umami tracking

--- app.vue
+++ app.vue
@@ -1,3 +1,24 @@
+<script setup>
+  const { isProduction } = useRuntimeConfig().public
+  if (isProduction) {
+    useHead({
+      script: [
+        {
+          src: '//js.hs-scripts.com/47847354.js',
+          async: true,
+          defer: true,
+        },
+        {
+          src: 'https://cloud.umami.is/script.js',
+          async: true,
+          defer: true,
+          'data-website-id': 'f8672b32-8925-4ae9-aed9-749620ab3dc9',
+        }
+      ],
+    })
+  }
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
