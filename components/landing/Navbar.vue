2144b3d54cc98c24878a20e1b797e0f97376da8b
Seth Brasile <seth.brasile@gmail.com>
2024-10-23 22:29:07 -0500

    adjust logo

--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -29,9 +29,19 @@ const open = ref(false);
   <LandingContainer>
     <header class="flex flex-col lg:flex-row justify-between items-center my-5">
       <div class="flex w-full lg:w-auto items-center justify-between">
-        <a href="/" class="text-lg"
-          ><span class="font-bold text-slate-800">Tenor</span
-          ><span class="text-slate-500">Creative</span>
+        <a href="/" class="text-lg content-center grid grid-cols-2 w-14">
+          <NuxtPicture
+            format="png"
+            src="/img/logo.png"
+            width="24"
+            height="24"
+            alt="Tenor"
+            class="w-6"
+          />
+          <div>
+            <span class="font-bold text-slate-800">Tenor</span>
+            <span class="text-slate-500">Creative</span>
+          </div>
         </a>
         <div class="block lg:hidden">
           <button @click="open = !open" class="text-gray-800">
