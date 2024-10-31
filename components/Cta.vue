5bebf51a56dd4cbb21a56d86f3db01ccd6ee283b
Seth Brasile <seth.brasile@gmail.com>
2024-10-31 11:17:17 -0500

    fix CTA text contrast

--- components/Cta.vue
+++ components/Cta.vue
@@ -12,7 +12,7 @@
     class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
   >
     <h2 class="text-white text-3xl md:text-6xl">You deserve a better website.</h2>
-    <p class="text-slate-500 mt-4 text-lg md:text-xl">
+    <p class="text-slate-300 mt-4 text-lg md:text-xl">
       Get in contact with us now to schedule a free consultation.
     </p>
     <div class="flex mt-5">
