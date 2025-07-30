256e6a26d37fa23960fe24fdeae1c058ef86c547
Seth Brasile <seth.brasile@gmail.com>
2025-07-30 01:05:39 -0500

    chat widget

--- app/app.vue
+++ app/app.vue
@@ -5,15 +5,22 @@ import { SpeedInsights } from '@vercel/speed-insights/nuxt';
   if (isProduction) {
     useHead({
       script: [
-        {
-          src: '//js.hs-scripts.com/47847354.js',
-          async: true,
-          defer: true,
-        },
+        // {
+        //   src: '//js.hs-scripts.com/47847354.js',
+        //   async: true,
+        //   defer: true,
+        // },
         {
           src: 'https://analytics.tenorcreative.com/script.js',
           defer: true,
           'data-website-id': '7fc6c441-cd5d-447f-a4a0-52eb991a1072',
+        },
+        {
+          src: 'https://widgets.leadconnectorhq.com/loader.js',
+          async: true,
+          defer: true,
+          'data-resources-url': 'https://widgets.leadconnectorhq.com/chat-widget/loader.js',
+          'data-widget-id': '6889b34067d91b0bae9c6673',
         }
       ],
     })
