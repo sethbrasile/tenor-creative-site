3d421eee57f89d009f172c99c6bdc8ec2a7c7c00
Seth Brasile <seth.brasile@gmail.com>
2025-07-31 18:18:21 -0500

    Update Footer.vue

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -13,26 +13,15 @@ const menuitems = [
 
 <template>
   <footer class="my-20 w-full">
-    <nav
-        class="w-full mt-2 flex"
-      >
-        <ul class="flex flex-row gap-3 m-auto">
-          <li v-for="item of menuitems">
-            <NuxtLink
-              :to="item.path"
-              class="flex px-3 py-2 text-gray-600 hover:text-gray-900"
-            >
-              {{ item.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-        <!-- <div class="lg:hidden flex items-center mt-3 gap-4">
-          <CtaLink href="#" styleName="muted" block size="md"
-            >Log in</CtaLink
-          >
-          <CtaLink href="#" size="md" block>Sign up</CtaLink>
-        </div> -->
-      </nav>
+    <nav class="w-full mt-2 flex">
+      <ul class="flex flex-row gap-3 m-auto">
+        <li v-for="item of menuitems">
+          <NuxtLink :to="item.path" class="flex px-3 py-2 text-gray-600 hover:text-gray-900">
+            {{ item.title }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
     <p class="text-center text-sm text-slate-500">
       Copyright © {{ new Date().getFullYear() }} Tenor Creative LLC. All rights reserved.
     </p>
