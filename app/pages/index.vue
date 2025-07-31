09262d8f0baf1ab064268f22b2261cef78e05bba
Seth Brasile <seth.brasile@gmail.com>
2025-07-31 15:56:24 -0500

    no 4 rs yet

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,28 +5,28 @@ const features = [
     description:
       "What are people saying about your business online? 72% of U.S. consumers said they research companies through social channels before making purchases.",
     icon: "mdi:thumbs-up",
-    slug: 'reputation',
+    slug: '',
   },
   {
     title: "Readiness",
     description:
       "Are you ready to convert leads into customers? Does your team capture 100% of leads? Studies show that 40%-65% of leads are missed simply because their call was missed and no one immediately followed up.",
     icon: "mdi:people-group",
-    slug: 'readiness',
+    slug: '',
   },
   {
     title: "Remarket/Resell",
     description:
       "Are you maximizing the value of your existing customer base? On average it costs 5 times more to acquire a new customer than it does to sell new services to an existing customer.",
     icon: "mdi:email",
-    slug: 'remarket',
+    slug: '',
   },
   {
     title: "Reach",
     description:
       "Where do you show up? 95.91% of all clicks occur on page 1 of Google, and the top three positions deliver 58.4% of traffic.",
     icon: "bxs:paper-plane",
-    slug: 'reach',
+    slug: '',
   },
 ];
 
@@ -39,7 +39,7 @@ useSeoMeta({
   <Container>
     <Hero></Hero>
     <CallOut />
-    <Features :features="features" url-base="the-4-rs"></Features>
+    <Features :features="features" url-base="/services"></Features>
     <!-- <Logos></Logos> -->
     <Cta location="Home"></Cta>
   </Container>
