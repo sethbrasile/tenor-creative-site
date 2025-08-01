63e104377a901b407037691b1c028626df3268e4
Seth Brasile <seth.brasile@gmail.com>
2025-07-31 19:00:28 -0500

    Update booking-activation.vue

--- app/pages/booking-activation.vue
+++ app/pages/booking-activation.vue
@@ -3,7 +3,7 @@
     <Sectionhead>
       <template v-slot:title>Book your service activation call</template>
       <template v-slot:desc>
-
+        We're done setting up! Hooray! Let's get together and go over everything we built!
       </template>
     </Sectionhead>
 
